339803efdaabf03c037b17339353d679d7c58769
kermekx <kermekx@gmail.com>
2016-02-11 20:53:01 +0100

    commande

--- src/com/outofskillsexception/hashcode/Commande.java
+++ src/com/outofskillsexception/hashcode/Commande.java
@@ -11,4 +11,27 @@ public class Commande {
 		demmande = items;
 	}
 	
+	public boolean isFinished() {
+		return remeaning() == 0;
+	}
+	
+	public int remeaning() {
+		int remeaning = 0;
+		
+		for (Integer nbr : demmande.values()) {
+			remeaning += nbr;
+		}
+		
+		return remeaning;
+	}
+	
+	public boolean delivry(Item item) {
+		if (demmande.get(item) == null || demmande.get(item) <= 0) {
+			return false;
+		}
+		
+		demmande.put(item, new Integer(demmande.get(item)) - 1);
+		return true;
+	}
+	
 }
