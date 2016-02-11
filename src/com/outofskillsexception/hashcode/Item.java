b52b8c92ac7aac8d991959b5844a8487da00bbb4
Laurent <laurent.poulin420@gmail.com>
2016-02-11 19:53:59 +0100

    Item fonc

--- src/com/outofskillsexception/hashcode/Item.java
+++ src/com/outofskillsexception/hashcode/Item.java
@@ -1,5 +1,27 @@
 package com.outofskillsexception.hashcode;
 
 public class Item {
+	private int weight,id;
+	
+	public Item(int id,int weight) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+		this.weight = weight;
+	}
 
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
