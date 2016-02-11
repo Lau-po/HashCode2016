46294ee649f2089a2ba12d648ec9992df83618dc
Affolaby Paul-Ivan <paulivanaffolabyn@hotmail.com>
2016-02-11 19:32:14 +0100

    Array of List

--- src/com/outofskillsexception/hashcode/CreateArray.java
+++ src/com/outofskillsexception/hashcode/CreateArray.java
@@ -1,20 +1,24 @@
 package com.outofskillsexception.hashcode;
 
+import java.util.ArrayList;
+
 public class CreateArray {
-	private Object[][] tab;
+	private ArrayList[][] tab;
 
 	public CreateArray(int x, int y) {
 		this.tab = tab;
-		tab = new Object[x][y];
+		tab = new ArrayList[x][y];
 	}
 
 	public void move(Object o, int n, int x) {
 		for (int i = 0; i < tab[0].length; i++) {
 			for (int j = 0; j < tab[1].length; j++) {
-				if (tab[i][j].equals(o)) {
-					Object tmp = tab[n][x];
-					tab[n][x] = o;
-					tab[i][x] = tmp;
+				if (tab[i][j].contains(o)) {
+					tab[n][x].add(o);
+					if (tab[i][j].isEmpty())
+						tab[i][j] = null;
+					else
+						tab[i][j].remove(o);
 				}
 			}
 		}
