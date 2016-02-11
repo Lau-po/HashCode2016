c8c4d12cd6f34372ea4724f712f848c635a3526d
Laurent <laurent.poulin420@gmail.com>
2016-02-11 19:50:24 +0100

    Drone fonc

--- src/com/outofskillsexception/hashcode/Drone.java
+++ src/com/outofskillsexception/hashcode/Drone.java
@@ -1,5 +1,49 @@
 package com.outofskillsexception.hashcode;
 
-public class Drone {
+import java.awt.Point;
 
+public class Drone {
+	private int weight,id;
+	private Point pos;
+	private Item item;
+	
+	public Drone(int id, int weight, int x, int y, Item item) {
+		// TODO Auto-generated constructor stub
+		this.weight = weight;
+		this.id = id;
+		this.pos = new Point(x,y);
+		this.item = item;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Point getPos() {
+		return pos;
+	}
+
+	public void setPos(int x, int y) {
+		this.pos.setLocation(x, y);;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
 }
