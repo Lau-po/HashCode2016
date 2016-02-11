32d53dd0e7d3c557a3c89c84becc50b19cb5ead5
Affolaby Paul-Ivan <paulivanaffolabyn@hotmail.com>
2016-02-11 19:59:52 +0100

    Pour toi Alex <3

--- src/com/outofskillsexception/hashcode/Commande.java
+++ src/com/outofskillsexception/hashcode/Commande.java
@@ -7,14 +7,61 @@ public class Commande {
 	private String name;
 	private String[] type;
 	private ArrayList<Commande> list;
+	private ArrayList<Drone> d;
 	
-	public Commande(int id, String name, String[] type){
+	public Commande(int id, String name, String[] type, ArrayList<Drone> d){
 		this.id = id;
 		this.name = name;
 		this.type = type;
+		this.d = d;
+		Drone free;
+		for(int i = 0; i < d.size(); i++){
+			if(d.get(i).isFree())
+				free = d.get(i);
+		}
 		this.list.add(new Commande(id, name, type));
 	}
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String[] getType() {
+		return type;
+	}
+
+	public void setType(String[] type) {
+		this.type = type;
+	}
+
+	public ArrayList<Commande> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<Commande> list) {
+		this.list = list;
+	}
+
+	public ArrayList<Drone> getD() {
+		return d;
+	}
+
+	public void setD(ArrayList<Drone> d) {
+		this.d = d;
+	}
+
 	public void delivered(Commande commande){
 		if(list.contains(commande))
 			list.remove(commande);
