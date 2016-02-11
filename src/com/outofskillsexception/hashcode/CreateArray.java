12798df8d33de0e7d72fd3fd61f35a371361a80d
Affolaby Paul-Ivan <paulivanaffolabyn@hotmail.com>
2016-02-11 19:24:04 +0100

    Update Move Array

--- src/com/outofskillsexception/hashcode/CreateArray.java
+++ src/com/outofskillsexception/hashcode/CreateArray.java
@@ -2,7 +2,21 @@ package com.outofskillsexception.hashcode;
 
 public class CreateArray {
 	private Object[][] tab;
-	public CreateArray(int x, int y){
+
+	public CreateArray(int x, int y) {
+		this.tab = tab;
 		tab = new Object[x][y];
+	}
+
+	public void move(Object o, int n, int x) {
+		for (int i = 0; i < tab[0].length; i++) {
+			for (int j = 0; j < tab[1].length; j++) {
+				if (tab[i][j].equals(o)) {
+					Object tmp = tab[n][x];
+					tab[n][x] = o;
+					tab[i][x] = tmp;
+				}
+			}
+		}
 	}
 }
