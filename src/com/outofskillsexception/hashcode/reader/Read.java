dfcf42c105cc8689ed769457e8e357f84caf8e5c
kermekx <kermekx@gmail.com>
2016-02-11 19:09:21 +0100

    Read first line

---
+++ src/com/outofskillsexception/hashcode/reader/Read.java
@@ -0,0 +1,57 @@
+package com.outofskillsexception.hashcode.reader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Read {
+	
+	private final File file;
+	
+	private int row;
+	private int column;
+	private int numberOfDrones;
+	private int deadline;
+	private int maxLoad;
+	
+	public Read(String file) {
+		this(new File(file));
+	}
+	
+	public Read(File file) {
+		this.file = file;
+	}
+	
+	/**
+	 * Read the input file
+	 */
+	public void run() {
+		
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+			
+			readFirstLine(bufferedReader.readLine());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * Read the first line
+	 */
+	public void readFirstLine(String line) throws Exception {
+		String[] params = line.split(" ");
+		
+		row = Integer.parseInt(params[0]);
+		column = Integer.parseInt(params[1]);
+		numberOfDrones = Integer.parseInt(params[2]);
+		deadline = Integer.parseInt(params[3]);
+		maxLoad = Integer.parseInt(params[4]);
+		
+		System.out.println(row + " " + column + " " + numberOfDrones + " " + deadline + " " + maxLoad);
+	}
+
+}
