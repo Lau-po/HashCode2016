e84e42428db4d94c861cdac261d5d0a51e66acdf
Affolaby Paul-Ivan <paulivanaffolabyn@hotmail.com>
2016-02-11 19:53:07 +0100

    Update delivered

--- src/com/outofskillsexception/hashcode/Commande.java
+++ src/com/outofskillsexception/hashcode/Commande.java
@@ -1,13 +1,22 @@
 package com.outofskillsexception.hashcode;
 
+import java.util.ArrayList;
+
 public class Commande {
 	private int id;
 	private String name;
 	private String[] type;
+	private ArrayList<Commande> list;
 	
 	public Commande(int id, String name, String[] type){
 		this.id = id;
 		this.name = name;
 		this.type = type;
+		this.list.add(new Commande(id, name, type));
+	}
+	
+	public void delivered(Commande commande){
+		if(list.contains(commande))
+			list.remove(commande);
 	}
 }
