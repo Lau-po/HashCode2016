be02ae6d124a0ff26ee8a19ee18c8cad73c57418
Affolaby Paul-Ivan <paulivanaffolabyn@hotmail.com>
2016-02-11 21:37:55 +0100

    Update Drone

--- src/com/outofskillsexception/hashcode/Drone.java
+++ src/com/outofskillsexception/hashcode/Drone.java
@@ -4,30 +4,30 @@ import java.awt.Point;
 import java.util.*;
 
 public class Drone {
-	private int weight,id;
+	private int weight, id;
 	private float timeOut;
 	private Point pos;
-	private List<Item> item = new ArrayList<Item>();
-	private boolean free= false;
+	private HashMap<Item, Integer> item = new HashMap<Item, Integer>();
+	private boolean free = false;
 
 	public Drone(int id, int weight, int x, int y) {
 		// TODO Auto-generated constructor stub
 		this.weight = weight;
 		this.id = id;
-		this.pos = new Point(x,y);
+		this.pos = new Point(x, y);
 	}
-	
-	public void setTimeOut(float i){
+
+	public void setTimeOut(float i) {
 		timeOut = i;
 	}
-	
-	public void update(){
-		timeOut-=1;
-		if(timeOut <= 0){
+
+	public void update() {
+		timeOut -= 1;
+		if (timeOut <= 0) {
 			setFree(true);
 		}
 	}
-	
+
 	public boolean isFree() {
 		return free;
 	}
@@ -57,22 +57,37 @@ public class Drone {
 	}
 
 	public void setPos(int x, int y) {
-		this.pos.setLocation(x, y);;
+		this.pos.setLocation(x, y);
+		;
 	}
 
-	public List<Item> getItems() {
+	public HashMap<Item, Integer> getItems() {
 		return item;
 	}
-	
+
 	public boolean haveItem(Item i) {
-		return item.contains(i);
+		return item.containsKey(i);
 	}
-	
+
 	public boolean removeItems(Item i) {
-		return item.remove((Object)i);
+		int qte = 0;
+		if(this.item.containsKey(i))
+			qte = this.item.get(i);
+		if(qte > 1){
+			qte--;
+			this.item.put(i, qte);
+		}
+		else
+			this.item.remove(i);
+		return true;
 	}
 
 	public void addItem(Item item) {
-		this.item.add(item);
+		int qte;
+		if (this.item.containsKey(item)) {
+			qte = this.item.get(item) + 1;
+		} else
+			qte = 1;
+		this.item.put(item, qte);
 	}
 }
